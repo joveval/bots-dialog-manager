ee9f6081e8c2f01ea670590d6679748153352833
jose_valenzuela_soto <valenzuelajoss@globalhitss.com>
2019-02-27 18:07:00 -0500

    ADD( Action static methods )

--- src/main/java/pe/joseval/bots/core/ActionTasks.java
+++ src/main/java/pe/joseval/bots/core/ActionTasks.java
@@ -6,4 +6,5 @@ public interface ActionTasks {
 	public static final String BUILD_ORDERED_RESPONSE_MESSAGES = "ordered_response_messages";
 	public static final String HANDLE_CONTEXT="handle_context";
 	public static final String RUN_CUSTOM_METHOD="run_custom_method";
+	public static final String CUSTOMIZE_DEFAULT_MESSAGE=StatesMachineConstants.CUSTOM_SIMPLE_MESSAGE;
 }
--- src/main/java/pe/joseval/bots/core/DmAction.java
+++ src/main/java/pe/joseval/bots/core/DmAction.java
@@ -1,20 +1,39 @@
 package pe.joseval.bots.core;
 
+import pe.joseval.bots.core.context.ContextHandler.ContextHandlingDefinition;
 import pe.joseval.util.states.machine.core.Action;
 import pe.joseval.util.states.machine.core.ActionType;
 
-public class DmAction extends Action{
+public class DmAction extends Action {
 
-	
 	public static DmAction action(ActionType actionType) {
+
 		DmAction dmAction = new DmAction();
 		dmAction.setActionType(actionType);
 		return dmAction;
 	}
-	
-	
-	public DmAction withResponseList(String ...posibleMessages) {
+
+	public DmAction withResponseList(String... posibleMessages) {
+
 		super.getCustomParams().put(ActionTasks.BUILD_RESPONSE_MESSAGE, posibleMessages);
+		return this;
+	}
+	
+	public DmAction withOrderedResponseList(String ... messages) {
+		
+		super.getCustomParams().put(ActionTasks.BUILD_ORDERED_RESPONSE_MESSAGES, messages);
+		return this;
+	}
+
+	public DmAction withContextHandlingTask(ContextHandlingDefinition def) {
+
+		super.getCustomParams().put(ActionTasks.HANDLE_CONTEXT, def);
+		return this;
+	}
+	
+	public DmAction withResponseStyleCustomizer(SimpleMessageCustomizer customizer) {
+		
+		super.getCustomParams().put(ActionTasks.CUSTOMIZE_DEFAULT_MESSAGE, customizer);
 		return this;
 	}
 }
